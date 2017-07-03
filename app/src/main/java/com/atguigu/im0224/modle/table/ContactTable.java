1b68debf55ec524b834a8ed4e6540a5c8195ebaa
tianxuewei1016 <93226539@qq.com>
2017-07-03 15:58:15 +0800

    创建邀请联系人表

--- app/src/main/java/com/atguigu/im0224/modle/table/ContactTable.java
+++ app/src/main/java/com/atguigu/im0224/modle/table/ContactTable.java
@@ -7,7 +7,11 @@ package com.atguigu.im0224.modle.table;
  */
 
 public class ContactTable {
-
+    /**
+     * 注意 ：
+     *      1 把sql语句一定要确定没有问题
+     *      2 数据库创建有问题的时候，如果需要重新创建 一定要卸载原来的应用
+     */
     public static final String TABLE_NAME = "contact";
     public static final String COL_USER_HXID = "userhxid";
     public static final String COL_USER_NAME = "username";
